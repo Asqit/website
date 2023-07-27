1945e9c97790b8afbbec834625cf5f1c590856fd
Ondřej Tuček <ondrejtucek9@gmail.com>
2023-07-27 20:55:10 +0200

    Fixing email submission
    Last point in my resolved issue was regarding the not working email form. The reason was, that deno deploy does not allow most common SMTP ports. So instead of using regular email service I am using ntfy.sh.

--- routes/api/mail.ts
+++ routes/api/mail.ts
@@ -1,5 +1,4 @@
 import { Handlers, Status } from "$fresh/server.ts";
-import { SMTPClient } from "https://deno.land/x/denomailer@1.6.0/mod.ts";
 import { config } from "https://deno.land/x/dotenv@v3.2.2/mod.ts";
 
 interface Payload {
@@ -11,32 +10,19 @@ export const handler: Handlers = {
   async POST(req: Request) {
     const env = config();
 
-    const { username, password, service, port, target } = env;
-
-    const client = new SMTPClient({
-      connection: {
-        hostname: service,
-        port: Number(port),
-        tls: true,
-        auth: {
-          username: username,
-          password: password,
-        },
-      },
-    });
+    const { ntfy_topic } = env;
 
     const payload: Payload | undefined = await req.json();
 
     if (payload) {
       try {
-        await client.send({
-          from: username,
-          to: target,
-          subject: `New email from ${payload.mail}`,
-          content: payload.message,
+        await fetch(`https://ntfy.sh/${ntfy_topic}`, {
+          method: "POST",
+          body: JSON.stringify({
+            title: `Portfolio: New message from ${payload.mail}`,
+            message: payload.message,
+          }),
         });
-
-        await client.close();
 
         return new Response("", { status: Status.OK });
       } catch {
